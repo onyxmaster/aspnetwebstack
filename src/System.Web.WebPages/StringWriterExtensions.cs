8ec1d8dc015ffef994fa311a83627c6cdc96fbba
Aristarkh Zagorodnikov <xm@drive.net>
2015-11-27 16:14:21 +0300

    Optimized common case of StringwriterExtensions.CopyTo

--- src/System.Web.WebPages/StringWriterExtensions.cs
+++ src/System.Web.WebPages/StringWriterExtensions.cs
@@ -3,12 +3,15 @@
 
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace System.Web.WebPages
 {
     internal static class StringWriterExtensions
     {
         public const int BufferSize = 1024;
+
+        private static readonly ThreadLocal<char[]> _bufferCache = new ThreadLocal<char[]>(() => new char[BufferSize]);
 
         // Used to copy data from a string writer to avoid allocating the full string
         // which can end up on LOH (and cause memory fragmentation).
@@ -17,9 +20,19 @@ namespace System.Web.WebPages
             StringBuilder builder = input.GetStringBuilder();
 
             int remainingChars = builder.Length;
+            if (remainingChars == 0)
+            {
+                return;
+            }
+            var outputWriter = output as StringWriter;
+            if (outputWriter != null)
+            {
+                var outputBuilder = outputWriter.GetStringBuilder();
+                outputBuilder.EnsureCapacity(outputBuilder.Length + remainingChars);
+            }
             int bufferSize = Math.Min(builder.Length, BufferSize);
 
-            char[] buffer = new char[bufferSize];
+            var buffer = _bufferCache.Value;
             int currentPosition = 0;
 
             while (remainingChars > 0)
