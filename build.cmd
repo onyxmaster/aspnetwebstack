38d7e37c7d6790a0d7bc7539cd097973a5ad6ddf
Aristarkh Zagorodnikov <xm@x-infinity.com>
2023-05-24 23:54:28 +0400

    Fix build

--- build.cmd
+++ build.cmd
@@ -24,19 +24,17 @@ if not exist %vswhere% (
   goto BuildFail
 )
 
-REM We're fine w/ any .NET SDK newer than 2.1.500 but also need a 2.1.x runtime. Microsoft.Net.Core.Component.SDK.2.1
 REM actually checks for only the runtime these days.
 set InstallDir=
 for /f "usebackq tokens=*" %%i in (`%vswhere% -version 17 -latest -prerelease -products * ^
     -requires Microsoft.Component.MSBuild ^
     -requires Microsoft.NetCore.Component.SDK ^
-    -requires Microsoft.Net.Core.Component.SDK.2.1 ^
     -property installationPath`) do (
   set InstallDir="%%i"
 )
 
 if not DEFINED InstallDir (
-  echo "Could not find a VS2019 installation with the necessary components (MSBuild, .NET Core 2.1 Runtime, .NET SDK). Please install VS2019 or the missing components."
+  echo "Could not find a VS2022 installation with the necessary components (MSBuild, .NET SDK). Please install VS2019 or the missing components."
 )
 
 if exist %InstallDir%\MSBuild\Current\Bin\MSBuild.exe (
